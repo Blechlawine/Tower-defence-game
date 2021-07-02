1ef7c75045942030af0493baa2c18a7d6479738c
Blechlawine <53613039+Blechlawine@users.noreply.github.com>
2021-07-02 15:59:48 +0200

    new Look for BasicProjectile

--- src/main/java/de/marc/towerDefenceGame/gameObjects/tower/projectile/projectiles/BasicProjectile.java
+++ src/main/java/de/marc/towerDefenceGame/gameObjects/tower/projectile/projectiles/BasicProjectile.java
@@ -5,13 +5,15 @@ import de.marc.towerDefenceGame.gameObjects.tower.projectile.Projectile;
 import de.marc.towerDefenceGame.utils.Color;
 import de.marc.towerDefenceGame.utils.GLUtils;
 import de.marc.towerDefenceGame.utils.Vector2;
+import org.lwjgl.opengl.GL11;
 
 public class BasicProjectile extends Projectile {
 
     private static double speed = 1d;
+    private final Color color = new Color("8BC34A");
 
     public BasicProjectile(Vector2 pos, Vector2 motion, double damage, long ttl) {
-        super(pos, motion, speed, damage, 0.5, ttl);
+        super(pos, motion, speed, damage, 1, ttl);
     }
 
     public static double getSpeed() {
@@ -20,7 +22,9 @@ public class BasicProjectile extends Projectile {
 
     @Override
     public void render() {
-        float lineWidth = 1 * (float)TowerDefenceGame.theGame.getPlayer().getScale();
-        GLUtils.drawLine(this.pos.getX(), this.pos.getY(), this.pos.getX() - this.motion.getX(), this.pos.getY() - this.motion.getY(), lineWidth, new Color("8BC34A"));
+        GL11.glPushMatrix();
+        GLUtils.rotateAroundLocation(this.motion.getAngleDeg(), this.pos);
+        GLUtils.drawRectCentered(this.pos.getX(), this.pos.getY(), this.size, this.size, this.color);
+        GL11.glPopMatrix();
     }
 }
